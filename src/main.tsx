79b67ca0ed5c363cabfdd5095c35401676dd53af
lunasales <lunasales0388@gmail.com>
2026-05-25 15:00:13 -0300

    fix: mudança na main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/inicio" element={<TelaInicial />} />
         <Route path="/contato" element={<Contato />} />
-        <Route path="/sobrenos" element={<SobreNos />} />
+        <Route path="/sobre_nos" element={<SobreNos />} />
         <Route path="/ajuda" element={<Ajuda />} />
         <Route path="/configuracoes" element={<Configuracoes />} />
         <Route path="/diario_emocional" element={<DiarioEmocional/> } />
